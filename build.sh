4f8274ec0492ac05f0722c3b874d6d6cfbaac2a8
Achilles <suhailhijry@gmail.com>
2022-07-05 06:09:44 +0300

    added more warnings to build output

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-g++ ./src/*.cpp -o ./build/metavm -fno-exceptions -fno-rtti -I./inc -I./inc/achilles -lbfd -ldl -Wall -D BACKWARD_HAS_BFD=1 -g3
-# g++ ./src/*.cpp -o ./build/metavm -fno-exceptions -fno-rtti -I./inc -I./inc/achilles -Wall -O3
+# g++ ./src/*.cpp -o ./build/metavm -fno-exceptions -fno-rtti -I./inc -I./inc/achilles -lbfd -ldl -W -Wall -D BACKWARD_HAS_BFD=1 -g3
+g++ ./src/main.cpp -o ./build/metavm -fno-exceptions -fno-rtti -I./inc -I./inc/achilles -W -Wall -O3 -g3
 
